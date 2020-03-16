fad450c594c28470f0c262c941594c3a4467e836
chase <chase.isabelle@vestiairecollective.com>
2020-03-16 13:14:05 +0100

    weeeeeeeeeeeeeeeeeeeeee!!

---
+++ example/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/chaseisabelle/backoff"
+	"github.com/chaseisabelle/backoff/expbo"
+	"github.com/chaseisabelle/backoff/linbo"
+	"time"
+)
+
+func main() {
+	initial := uint64(1000) //<< start at 1 sec (1000ms)
+	max := uint64(1000 * 10) //<< cap it at 10 seconds
+	exp := uint64(2) //<< exponent
+	inc := uint64(1000) //<< increment 1 second
+
+	expBO, err := expbo.New(initial, max, exp)
+
+	if err != nil {
+		panic(err)
+	}
+
+	linBO, err := linbo.New(initial, max, inc)
+
+	if err != nil {
+		panic(err)
+	}
+
+	flatBO, err := backoff.New(initial, initial, max, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			println("exponential backoff start...")
+
+			expBO.Backoff()
+
+			println("...exponential backoff stop")
+		}
+	}()
+
+	go func() {
+		for {
+			println("linear backoff start...")
+
+			linBO.Backoff()
+
+			println("...linear backoff stop")
+		}
+	}()
+
+	go func() {
+		for {
+			println("flat backoff start...")
+
+			flatBO.Backoff()
+
+			println("...flat backoff stop")
+		}
+	}()
+
+	for {
+		time.Sleep(time.Duration(10) * time.Second) //<< reset after 10 seconds
+
+		expBO.Reset()
+		linBO.Reset()
+		flatBO.Reset()
+
+		println("reset all")
+	}
+}
---
+++ linbo/linbo.go
@@ -0,0 +1,11 @@
+package linbo
+
+import (
+	"github.com/chaseisabelle/backoff"
+)
+
+func New(initial uint64, max uint64, inc uint64) (*backoff.Backoff, error) {
+	return backoff.New(initial, initial, max, func(current uint64) uint64 {
+		return current + inc
+	})
+}
